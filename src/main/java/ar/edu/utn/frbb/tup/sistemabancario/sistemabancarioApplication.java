a8598bf4293d2670baa9af6d14204b36e8df170e
DiegoXeneize <18diegobruno18@gmail.com>
2024-09-07 19:39:50 -0300

    add changes

---
+++ src/main/java/ar/edu/utn/frbb/tup/sistemabancario/sistemabancarioApplication.java
@@ -0,0 +1,9 @@
+package ar.edu.utn.frbb.tup.sistemabancario;
+
+public class sistemabancarioApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
